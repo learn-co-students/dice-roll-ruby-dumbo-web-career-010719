8cbb10fad52d783b3428b9b7d60eb556c4bcd93e
zaheer <m.abbaszaheer@gmail.com>
2018-12-20 00:53:35 -0500

    dice_roll:rb

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,12 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  rand(1 .. 6)
 end
+
+def roll_array
+ array =[1,2,3,4,5,6]
+
+  random_number=1 + rand(array.count)
+end
+puts roll_array
